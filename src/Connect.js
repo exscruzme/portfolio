a16a703a58468a6e83ab2f07464684bb5a2ff1e4
Stephanie Cruz <exscruzme@gmail.com>
2018-11-16 16:17:50 -0500

    added cards to connect

--- src/Connect.js
+++ src/Connect.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {Col, Card, CardTitle, Icon} from 'react-materialize';
+import amex from './amex_logo.png';
 
 class Connect extends Component {
 	render() {
@@ -6,8 +8,27 @@ class Connect extends Component {
 			<div>
 				<main>
 					<section><h1>Connect</h1></section>
-					<section><h3>GitHub / Bitbucket </h3></section>
-					<section><h3>LinkedIn</h3></section>
+					<section>
+						<Col l ={3} m={3} s={12}>
+						    <Card className='white darken-1' textClassName='black-text' title={['Github ', <span><Icon>photo</Icon></span>]} actions={[<a href='https://github.com/exscruzme'>github.com/exscruzme</a>]}>
+						    Check out what I've been working on.
+						    </Card>
+						</Col>
+					</section>
+					<section>
+						<Col l ={3} m={3} s={12}>
+						    <Card className='white darken-1' textClassName='black-text' title={['LinkedIn ', <span><Icon>photo</Icon></span>]} actions={[<a href='https://linkedin.com/in/exscruzme'>linkedin.com/in/exscruzme</a>]}>
+						    Check out what I've been working on.
+						    </Card>
+						</Col>
+					</section>
+					<section>
+						<Col l ={3} m={3} s={12}>
+						    <Card className='white darken-1' textClassName='black-text' title={['Email ', <span><Icon>photo</Icon></span>]} actions={[<a href='exscruzme@gmail.com'>exscruzme@gmail.com</a>]}>
+						    Check out what I've been working on.
+						    </Card>
+						</Col>
+					</section>
 				</main>
 			</div>
 		);
